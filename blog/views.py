d406860a99ba04ef72b8469b50c5ed2b59507bf5
Edb83 <ed3radley@gmail.com>
2021-11-12 15:26:40 +0000

    Deployment commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -56,6 +57,7 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.success(request, "Thanks for commenting! Your message will appear when it has been approved by a site moderator.")
         
         else:
             comment_form = CommentForm()
